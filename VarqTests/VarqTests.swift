56d16c3328d9790acec37f7c251eaf938fa8fd16
Pratik Rai <raipratik0101@gmail.com>
2026-07-21 03:23:13 +0530

    Initial Commit

---
+++ VarqTests/VarqTests.swift
@@ -0,0 +1,19 @@
+//
+//  VarqTests.swift
+//  VarqTests
+//
+//  Created by Pratik Rai on 21/07/26.
+//
+
+import Testing
+@testable import Varq
+
+struct VarqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
